344a65f71727e808f2eebe61c1215021b25777b5
Alex Hickel <ahickel@madisoncollege.edu>
2023-09-24 21:18:38 -0500

    Submitting Milstone #3 for Grading

--- apache_log_analyzer.py
+++ apache_log_analyzer.py
@@ -11,18 +11,20 @@ CourseDescription= "Welcome! This program will help you to parse information fro
 print(CourseDescription)
 
 #Here I assign the user's input to the variable Continue
-Continue = input("Would you like to continue? Please reply with Y or N for yes or no.")
+#Continue = input("Would you like to continue? Please reply with Y or N for yes or no.")
 
 #This line prints for the user a recognition that their entry was received
-print(f"Thank you for your input, {Continue}")
+#print(f"Thank you for your input, {Continue}")
 
-#converting apache log info into a string and attaching it to a variable
-apache_log_address = '111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)"'
+#converting apache log info into a single string, while making sure to add in new lines using \n for each entry, and attaching the log entries to a single variable.
+apache_logs ='111.222.333.123 HOME - [01/Feb/1998:01:08:39 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 198 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)" \n111.222.333.124 HOME - [01/Feb/1998:01:08:46 -0800] "GET /bannerad/ad.htm HTTP/1.0" 200 28083 "http://www.referrer.com/bannerad/ba_intro.htm" "Mozilla/4.01 (Macintosh; I; PPC)" \n111.222.333.125 AWAY - [01/Feb/1998:01:08:53 -0800] "GET /bannerad/ad7.gif HTTP/1.0" 401 9332 "http://www.referrer.com/bannerad/ba_ad.htm" "Mozilla/4.01 (Macintosh; I; PPC)" \n111.222.333.126 AWAY - [01/Feb/1998:01:09:14 -0800] "GET /bannerad/click.htm HTTP/1.0" 501 207 "http://www.referrer.com/bannerad/menu.htm" "Mozilla/4.01 (Macintosh; I; PPC)"'
 
-#Slice code to isolate IP address from the beginning of the string, and print it
-slice_ip = apache_log_address[0:15]
-print(f"Log request from:{slice_ip:*^22}")
+#This code converts each log entry into a list, using the split function, by seperating each entry with the \n break.
+seperated_log_list = apache_logs.split("\n")
 
-#Using split function to print the return code value from the apache log string
-split_log = apache_log_address.split()
-print(f"Return Code: {split_log[8]}")
+#creating a 'for' loop to isolate each log entry while also slicing code to print the isolated IP address from the beginning of the string. We're also using split function to print the return code value from the isolated apache log string, which has been converted into a list within the loop.
+for log in seperated_log_list:
+    slice_ip = log[0:15]
+    print(f"Log request from: {slice_ip:*^22}")
+    split_log = log.split()
+    print(f"Return Code: {split_log[8]}")
